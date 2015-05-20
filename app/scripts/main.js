3451fbd4e6858367a1f62810f39895f4974befb0
Waog <oliver.stadie@gmail.com>
2015-05-20 03:59:38 +0200

    :books: ADDED: github library and underscore
    - Dynamically adding cards from hard wired github issues now.
    - Card initialization is done when adding a card now.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,37 +2,16 @@ console.log('\'Allo \'Allo!');
 
 feama = {};
 
-feama.initBoard = function() {
-    $(".column").sortable({
+feama.addColumn = function() {
+    $(".board").append('<div class="column"></div>');
+
+    $(".board > .column:last-child").sortable({
         connectWith : ".column",
         handle : ".portlet-header",
         cancel : ".portlet-toggle",
-        placeholder : "portlet-placeholder ui-corner-all"
-    });
-
-    $(".column").sortable({
+        placeholder : "portlet-placeholder ui-corner-all",
         containment : ".board"
     });
-
-    $(".portlet")
-            .addClass(
-                    "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all")
-            .find(".portlet-header")
-            .addClass("ui-widget-header ui-corner-all")
-            .prepend(
-                    "<span class='ui-icon ui-icon-plusthick portlet-toggle'></span>");
-
-    $(".portlet-content").hide();
-
-    $(".portlet-toggle").click(function() {
-        var icon = $(this);
-        icon.toggleClass("ui-icon-minusthick ui-icon-plusthick");
-        icon.closest(".portlet").find(".portlet-content").toggle();
-    });
-};
-
-feama.addColumn = function() {
-    $(".board").append('<div class="column"></div>');
 };
 
 feama.addFeature = function(columnIndex, header, content) {
@@ -46,21 +25,58 @@ feama.addCard = function(columnIndex, header, content) {
 feama.addGenericCard = function(columnIndex, header, content, isFeature) {
     var featureClassString = isFeature ? " feature" : "";
 
-    $(".column:eq(" + columnIndex + ")").append(
-            '<div class="portlet' + featureClassString + '"></div>');
-    $('.column:eq(' + columnIndex + ') > div:last-child').append(
-            '<div class="portlet-header">' + header + '</div>');
-    $('.column:eq(' + columnIndex + ') > div:last-child').append(
-            '<div class="portlet-content">' + content + '</div>');
+    var column = $(".column:eq(" + columnIndex + ")");
+
+    column.append('<div class="portlet' + featureClassString + '"></div>');
+    var portlet = column.children('div.portlet:last-child');
+
+    portlet.append('<div class="portlet-header">' + header + '</div>');
+    portlet.append('<div class="portlet-content">' + content + '</div>');
+
+    portlet
+            .addClass(
+                    "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all")
+            .find(".portlet-header")
+            .addClass("ui-widget-header ui-corner-all")
+            .prepend(
+                    "<span class='ui-icon ui-icon-plusthick portlet-toggle'></span>");
+
+    portlet.find(".portlet-content").hide();
+
+    portlet.find('.portlet-toggle').click(function() {
+        var icon = $(this);
+        icon.toggleClass("ui-icon-minusthick ui-icon-plusthick");
+        icon.closest(".portlet").find(".portlet-content").toggle();
+    });
 };
 
 $(function() {
 
-    for (i = 0; i < 5; i++) {
-        feama.addColumn();
-        feama.addFeature(i, "My Feature " + i, "My Verbose Content");
-        feama.addCard(i, "My Card A" + i, "My Verbose Content");
-        feama.addCard(i, "My Card B" + i, "My Verbose Content");
-    }
-    feama.initBoard();
+    var github = new Github({
+    // username : "YOU_USER",
+    // password : "YOUR_PASSWORD",
+    // auth : "basic"
+    });
+
+    var issues = github.getIssues("Waog", "towelGuy");
+
+    var options = {};
+
+    issues.list(options, function(err, issues) {
+        console.log('err:', err);
+        console.log('issues:', issues);
+
+        for (var i = 0; i < 5; i++) {
+            feama.addColumn();
+        }
+        for (var i = 0; i < issues.length; ++i) {
+            if (i in issues) {
+                var issue = issues[i];
+                console.log('issue' + i + ":", issue);
+                console.log('title' + i + ":", issue.title);
+                console.log('issue' + i + ":", issue.body);
+                feama.addCard(i % 5, issue.title, issue.body);
+            }
+        }
+    });
 });
