84d59dfc52bdbf9d2cf90c8616d431c7784c7064
Waog <oliver.stadie@gmail.com>
2015-05-20 18:42:08 +0200

    :droplet: removed jshint warnings + code formating

--- app/scripts/login.js
+++ app/scripts/login.js
@@ -1,51 +1,60 @@
+'use strict';
+
+/* global hello */
+
 var stomapLogin = {};
 
 stomapLogin.handleError = function(e) {
-    console.log('authorization error: ', e)
-}
+	console.log('authorization error: ', e);
+};
 
 stomapLogin.handleAuthLogin = function(auth) {
 
-    console.log("auth.login success #1: ", auth);
+	console.log('auth.login success #1: ', auth);
 
-    $('#profile').empty();
-    $('#login').empty();
-    $('#logout').empty();
-    $('#logout').append('<button onclick="hello(\'github\').logout()">logout</button>');
+	$('#profile').empty();
+	$('#login').empty();
+	$('#logout').empty();
+	$('#logout').append(
+	    '<button onclick="hello(\'github\').logout()">logout</button>');
 
-    // Call user information, for the given network
-    hello(auth.network).api('/me').then(stomapLogin.handleMeResponse,
-            stomapLogin.handleError);
-}
+	// Call user information, for the given network
+	hello(auth.network).api('/me').then(stomapLogin.handleMeResponse,
+	    stomapLogin.handleError);
+};
 
 stomapLogin.handleAuthLogout = function(auth) {
-    
-    console.log("auth.logout success #1: ", auth);
-    
-    // Call user information, for the given network
-    $('#profile').empty();
-    $('#login').empty();
-    $('#logout').empty();
-    $('#logout').append('<button onclick="hello(\'github\').login()">login</button>');
-}
+
+	console.log('auth.logout success #1: ', auth);
+
+	// Call user information, for the given network
+	$('#profile').empty();
+	$('#login').empty();
+	$('#logout').empty();
+	$('#logout').append(
+	    '<button onclick="hello(\'github\').login()">login</button>');
+};
 
 stomapLogin.handleMeResponse = function(r) {
 
-    console.log('logged in as ' + r.name, r);
+	console.log('logged in as ' + r.name, r);
 
-    $('#profile').empty();
-    $('#profile').append(r.name);
-    $('#profile').append('<img style="height: 50px; width: auto;" src="' + r.thumbnail + '" />');
-}
+	$('#profile').empty();
+	$('#profile').append(r.name);
+	$('#profile').append(
+	    '<img style="height: 50px; width: auto;" src="' + r.thumbnail + '" />');
+};
 
 hello.init({
-    // google :
-    // '707503277953-0l52gh27j9dbpaoubeec5kqsupvirn2h.apps.googleusercontent.com',
-    github : '1418f544c5ed81d99e77'
+	// google :
+	// '707503277953-0l52gh27j9dbpaoubeec5kqsupvirn2h.apps.googleusercontent.com',
+	github : '1418f544c5ed81d99e77'
 }, {
     /* display: 'page', */
+    /* jshint camelcase: false */
     redirect_uri : 'http://localhost:9000/',
     oauth_proxy : 'https://auth-server.herokuapp.com/proxy'
+/* jshint camelcase: true */
 });
 
 hello.on('auth.login', stomapLogin.handleAuthLogin, stomapLogin.handleError);
