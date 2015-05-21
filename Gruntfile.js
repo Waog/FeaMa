5a866622f2a0c0e960fc11be65d198a20a4ced4c
Waog <oliver.stadie@gmail.com>
2015-05-21 22:21:29 +0200

    :package: replaced load-grunt-tasks by jit-grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,8 +13,8 @@ module.exports = function (grunt) {
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
 
-  // Load grunt tasks automatically
-  require('load-grunt-tasks')(grunt);
+  // Load grunt tasks automatically & lazy load tasks
+  require('jit-grunt')(grunt);
 
   // Configurable paths
   var config = {
