0d942c7ebf80fc063ba0f0823c11fc60f6ea5138
Waog <oliver.stadie@gmail.com>
2015-05-24 03:09:46 +0200

    :package::books: partitially introduced TeddyMocks

--- test/spec/FirstTsTest.ts
+++ test/spec/FirstTsTest.ts
@@ -26,4 +26,37 @@ describe('Calculations Unit Tests:',() => {
             done();
         });
     });
+
+    describe('TeddyMocks',() => {
+        it('should Stub objects like described in their readme',(done) => {
+            var expected = 123;
+            var stub = new TeddyMocks.Stub<GithubApi.GithubLogin>(GithubApi.GithubLogin);
+
+            stub.stubs(m => m.getHello()).andReturns(null);
+            expect(stub.object.getHello()).to.equal(null);
+
+            expect(stub.assertsThat(s => s.getHello()).wasCalled()).to.equal(true);
+
+            done();
+        });
+
+        it('should replace globals with GlobalStub',(done) => {
+            
+//            var xmlhttp = new XMLHttpRequest();
+//            
+//            expect(xmlhttp.send).to.be.instanceOf(Function);
+//            
+//            TeddyMocks.GlobalOverride.createScope(() => {
+//
+//                var globalStub = new TeddyMocks.GlobalStub<XMLHttpRequest>("XMLHttpRequest");
+//                globalStub.stubs(s => s.send(undefined), false);
+//
+//                var request = new XMLHttpRequest();
+//                request.send(undefined);
+//
+//                expect(globalStub.assertsThat(s => s.send(undefined)).wasCalled()).to.equal(true);
+//            });
+            done();
+        });
+    });
 });
