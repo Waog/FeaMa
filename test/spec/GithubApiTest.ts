5843f62e36151c1aa21e95c4065fb6ff827b206f
Waog <oliver.stadie@gmail.com>
2015-05-26 16:04:16 +0200

    :herb: ADDED new GithubIssue Test

--- test/spec/GithubApiTest.ts
+++ test/spec/GithubApiTest.ts
@@ -15,7 +15,6 @@ module GithubApi {
 
         beforeEach = () => {
             this.loginHandler = new TeddyMocks.Stub<UserLoginHandler>(UserLoginHandlerMock);
-            this.loginHandler = new TeddyMocks.Stub<UserLoginHandler>(TeddyMocks.Stub);
             this.githubLogin = new GithubLogin(this.loginHandler.object);
             this.loginHandler.stubs(m => m.handleUserLogin(this.githubLogin));
             expect(this.githubLogin).to.be.ok;
@@ -39,16 +38,92 @@ module GithubApi {
             });
         }
     }
-    
+
     export class GithubIssueTest extends WaogTest.ClassTest {
 
         target: GithubIssue = null;
 
+        githubResponse = {
+            "url": "https://api.github.com/repos/Waog/sandboxRepo/issues/12",
+            "labels_url": "https://api.github.com/repos/Waog/sandboxRepo/issues/12/labels{/name}",
+            "comments_url": "https://api.github.com/repos/Waog/sandboxRepo/issues/12/comments",
+            "events_url": "https://api.github.com/repos/Waog/sandboxRepo/issues/12/events",
+            "html_url": "https://github.com/Waog/sandboxRepo/issues/12",
+            "id": 79036412,
+            "number": 12,
+            "title": "stomap",
+            "user": {
+                "login": "Waog",
+                "id": 5098472,
+                "avatar_url": "https://avatars.githubusercontent.com/u/5098472?v=3",
+                "gravatar_id": "",
+                "url": "https://api.github.com/users/Waog",
+                "html_url": "https://github.com/Waog",
+                "followers_url": "https://api.github.com/users/Waog/followers",
+                "following_url": "https://api.github.com/users/Waog/following{/other_user}",
+                "gists_url": "https://api.github.com/users/Waog/gists{/gist_id}",
+                "starred_url": "https://api.github.com/users/Waog/starred{/owner}{/repo}",
+                "subscriptions_url": "https://api.github.com/users/Waog/subscriptions",
+                "organizations_url": "https://api.github.com/users/Waog/orgs",
+                "repos_url": "https://api.github.com/users/Waog/repos",
+                "events_url": "https://api.github.com/users/Waog/events{/privacy}",
+                "received_events_url": "https://api.github.com/users/Waog/received_events",
+                "type": "User",
+                "site_admin": false
+            },
+            "labels": [
+                {
+                    "url": "https://api.github.com/repos/Waog/sandboxRepo/labels/bug",
+                    "name": "bug",
+                    "color": "fc2929"
+                },
+                {
+                    "url": "https://api.github.com/repos/Waog/sandboxRepo/labels/duplicate",
+                    "name": "duplicate",
+                    "color": "cccccc"
+                },
+                {
+                    "url": "https://api.github.com/repos/Waog/sandboxRepo/labels/enhancement",
+                    "name": "enhancement",
+                    "color": "84b6eb"
+                }
+            ],
+            "state": "open",
+            "locked": false,
+            "assignee": {
+                "login": "Waog",
+                "id": 5098472,
+                "avatar_url": "https://avatars.githubusercontent.com/u/5098472?v=3",
+                "gravatar_id": "",
+                "url": "https://api.github.com/users/Waog",
+                "html_url": "https://github.com/Waog",
+                "followers_url": "https://api.github.com/users/Waog/followers",
+                "following_url": "https://api.github.com/users/Waog/following{/other_user}",
+                "gists_url": "https://api.github.com/users/Waog/gists{/gist_id}",
+                "starred_url": "https://api.github.com/users/Waog/starred{/owner}{/repo}",
+                "subscriptions_url": "https://api.github.com/users/Waog/subscriptions",
+                "organizations_url": "https://api.github.com/users/Waog/orgs",
+                "repos_url": "https://api.github.com/users/Waog/repos",
+                "events_url": "https://api.github.com/users/Waog/events{/privacy}",
+                "received_events_url": "https://api.github.com/users/Waog/received_events",
+                "type": "User",
+                "site_admin": false
+            },
+            "milestone": null,
+            "comments": 0,
+            "created_at": "2015-05-21T15:06:11Z",
+            "updated_at": "2015-05-22T23:33:34Z",
+            "closed_at": null,
+            "body": "foo STOMAP STOMAP STOMAP STOMAP STOMAP STOMAP STOMAP STOMAP STOMAP",
+            "closed_by": null
+        }
+
         constructorTest = () => {
             it('should work properly',() => {
-            	
-            	this.target = new GithubIssue(null, null);
-            	
+
+                var ghLoginMock = new TeddyMocks.Stub<GithubLogin>(GithubLogin);
+                this.target = new GithubIssue(this.githubResponse, ghLoginMock.object);
+
                 expect(this.target).to.be.ok;
                 expect(this.target).to.be.a.instanceOf(GithubIssue);
             });
