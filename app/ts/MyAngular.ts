5ca959ac0b0ccd5716ae46e87c2199478e9cca48
Waog <oliver.stadie@gmail.com>
2015-06-02 20:05:10 +0200

    :herbs: made angular jquery ui and bootstrap work together

--- app/ts/MyAngular.ts
+++ app/ts/MyAngular.ts
@@ -3,30 +3,32 @@
 import {Component, View, bootstrap, For, If} from 'angular2/angular2';
 
 @Component({
-  selector: 'child'
+    selector: 'child'
 })
 @View({
-  templateUrl: 'temp.html'
+    templateUrl: 'temp.html'
 })
 class ChildComponent {
-  message: string;
-  constructor() {
-    this.message = "I'm the child";
-  }
+    message: string;
+    constructor() {
+        this.message = "I'm the child";
+    }
 }
 
 @Component({
-  selector: 'stomap'
+    selector: 'stomap'
 })
 @View({
-  templateUrl: 'someTemplate.html',
-  directives: [ChildComponent]
+    templateUrl: 'someTemplate.html',
+    directives: [ChildComponent]
 })
 class ParentComponent {
-  message: string;
-  
-  constructor() {
-    this.message = "I'm the parent";
-  }
+    message: string;
+
+    constructor() {
+        this.message = "I'm the parent";
+        console.log($('.boardcard'));
+        $('.boardcard').draggable();
+    }
 }
 bootstrap(ParentComponent);
